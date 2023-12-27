957ec157536086042ebb52cbd57d021d0a5a3b55
iservng <iservng@gmail.com>
2023-12-27 13:24:35 +0100

    I have successfuly create a Message-component and tested it

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,17 @@
+/**
+ * THE MESSAGE COMPONENT
+ * --------------------------
+ * In the "src" folder create "Message.tsx" file
+ * 
+ * The extension of typeScript files should be ".ts" or ".tsx". ".ts" is used for ordinary typeScript files while ".tsx" is ued for react-component-files. 
+ * 
+ * There are two ways to create a react component, we can use a Javasript-class or a function, using Javascript-function is recommended because is more concise and easier to write.
+ * 
+ * This component will be used in the App-component.
+ */
+function Message() 
+{
+    return <h1>Hello World</h1>
+
+}
+export default Message;
